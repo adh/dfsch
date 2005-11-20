ed171790736e7dc0a2ac95c0789236e013cfb7e5
Ales Hakl <ales@hakl.net>
2005-11-20 14:52:51 +0000

    git-svn-id: svn://mahoro/dfsch/trunk@135 876e91b9-e3f0-0310-bb80-9da7c39ee78d

--- scm/test.scm
+++ scm/test.scm
@@ -73,6 +73,18 @@
       (cond ((assoc 'b '((a 1) (b 2))) => cadr)
             (else ()))
       2)
+(delimiter)
+(test 'caseSimple
+      (case (* 2 3)
+        ((2 3 5 7) 'prime)
+        ((1 4 6 8 9) 'composite))
+      'composite)
+(test 'caseElse
+      (case (car '(c d))
+        ((a e i o u) 'vowel)
+        ((w y) 'semivowel)
+        (else 'consonant))
+      'consonant)
 
 (delimiter)
 
