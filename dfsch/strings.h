d7a0532b425a6d1f6f4869d1ac973a245607e65e
Ales Hakl <ales@hakl.net>
2006-01-05 12:06:04 +0000

    2006-01-05  Ales Hakl  <ales at hakl dot net>
    +	* dfsch/strings.h: Some basic idea of API.
    +
     	* src/strings.c dfsch/strings.h: Files for new implementation of
     	string handling
     
    
    
    git-svn-id: svn://mahoro/dfsch/trunk@169 876e91b9-e3f0-0310-bb80-9da7c39ee78d

---
+++ dfsch/strings.h
@@ -0,0 +1,22 @@
+#ifndef H__dfsch__strings__
+#define H__dfsch__strings__
+
+#include <dfsch/dfsch.h>
+
+typedef struct dfsch_strbuf_t{
+  char* ptr;
+  size_t len;
+} dfsch_strbuf_t;
+
+extern dfsch_strbuf_t* dfsch_strbuf_create(char* ptr, size_t len);
+
+extern dfsch_object_t* dfsch_make_string_cstr(char* string);
+extern dfsch_object_t* dfsch_make_string_strbuf(dfsch_strbuf_t* strbuf);
+extern dfsch_object_t* dfsch_make_string_buf(char* ptr, size_t len);
+
+extern char* dfsch_string_to_cstr(dfsch_object_t* obj);
+extern dfsch_strbuf_t* dfsch_string_to_buf(dfsch_object_t* obj);
+
+// TODO: some basic opertations: concatenation, indexing...
+
+#endif
