357c84f3ffcbd252735d3711cdbd7eaedaf5e5a4
Ales Hakl <ales@hakl.net>
2006-05-12 12:09:06 +0000

    2006-05-12  Ales Hakl  <ales at hakl dot net>
    +	* scm/ObjectFramework.scm (define-struct): Structures (preliminary)
    +
     	* src/native.c: for-each and map functions
     	map works
     
    
    
    git-svn-id: svn://mahoro/dfsch/trunk@345 876e91b9-e3f0-0310-bb80-9da7c39ee78d

--- scm/ObjectFramework.scm
+++ scm/ObjectFramework.scm
@@ -6,5 +6,36 @@
 	  (lambda (,(gensym) ,@(cdr signature))
 	    ,@body)))))))
 
-
+(define-macro (define-struct name slots)
+  (define num-slots (length slots))
+  `((define (,(string->symbol (string-append "make-" (symbol->string name))))
+      (make-vector ,num-slots #n))
+    ,@(let ((offset 0))
+	(map 
+	 (lambda (i)
+	   (define func
+	     `(define (,(string->symbol (string-append (symbol->string name)
+						       "-"
+						       (symbol->string i)
+						       "-set!"))
+		       struct value)
+		(vector-set! struct ,offset value)))
+	   (set! offset (+ 1 offset))
+	   func)
+	 slots)
+	)
+    ,@(let ((offset 0))
+	(map 
+	 (lambda (i)
+	   (define func
+	     `(define (,(string->symbol (string-append (symbol->string name)
+						       "-"
+						       (symbol->string i)
+						       "-ref"))
+		       struct)
+		(vector-ref struct ,offset)))
+	   (set! offset (+ 1 offset))
+	   func)
+	 slots)
+	)))
 				   
