cd6d1bd230931a84eafd2eb5a5363df9c03d7a69
Ales Hakl <ales@hakl.net>
2006-05-13 15:42:56 +0000

    git-svn-id: svn://mahoro/dfsch/trunk@347 876e91b9-e3f0-0310-bb80-9da7c39ee78d

--- scm/lib.scm
+++ scm/lib.scm
@@ -2,40 +2,73 @@
   (make-macro 
    (lambda (env signature . body)
      `((define ,(car signature) 
-	 (make-macro
-	  (lambda (,(gensym) ,@(cdr signature))
-	    ,@body)))))))
+         (make-macro
+          (lambda (,(gensym) ,@(cdr signature))
+            ,@body)))))))
 
 (define-macro (define-struct name slots)
   (define num-slots (length slots))
-  `((define (,(string->symbol (string-append "make-" (symbol->string name))))
-      (make-vector ,num-slots #n))
+  
+  (define slot-names (map 
+                      (lambda (slot-desc) 
+                        (if (pair? slot-desc)
+                            (car slot-desc)
+                            slot-desc))
+                      slots))
+
+  (define slot-defaults (map 
+                         (lambda (slot-desc)
+                           (if (pair? slot-desc)
+                               (car (cdr slot-desc))
+                               #n))
+                         slots))
+
+  `((define (,(string->symbol (string-append "make-" (symbol->string name)))
+             . args)
+      (define self (list->vector ',slot-defaults))
+      (let loop ((i args))
+        (if (pair? i)
+            (let ((name (car i)) (value (car (cdr i))))
+              (vector-set! self 
+                           (case name
+                             ,@(let ((offset 0))
+                                 (map 
+                                  (lambda (field)
+                                    (define cls `((,field) ,offset))
+                                    (set! offset (+ 1 offset))
+                                    cls)
+                                  slot-names))
+                             (else (throw 'struct:no-such-slot)))
+                           value)
+              (loop (cdr (cdr i))))
+            ()))
+      self)
     ,@(let ((offset 0))
-	(map 
-	 (lambda (i)
-	   (define func
-	     `(define (,(string->symbol (string-append (symbol->string name)
-						       "-"
-						       (symbol->string i)
-						       "-set!"))
-		       struct value)
-		(vector-set! struct ,offset value)))
-	   (set! offset (+ 1 offset))
-	   func)
-	 slots)
-	)
+        (map 
+         (lambda (field)
+           (define func
+             `(define (,(string->symbol (string-append (symbol->string name)
+                                                       "-"
+                                                       (symbol->string field)
+                                                       "-set!"))
+                       struct value)
+                (vector-set! struct ,offset value)))
+           (set! offset (+ 1 offset))
+           func)
+         slot-names)
+        )
     ,@(let ((offset 0))
-	(map 
-	 (lambda (i)
-	   (define func
-	     `(define (,(string->symbol (string-append (symbol->string name)
-						       "-"
-						       (symbol->string i)
-						       "-ref"))
-		       struct)
-		(vector-ref struct ,offset)))
-	   (set! offset (+ 1 offset))
-	   func)
-	 slots)
-	)))
-				   
+        (map 
+         (lambda (field)
+           (define func
+             `(define (,(string->symbol (string-append (symbol->string name)
+                                                       "-"
+                                                       (symbol->string field)
+                                                       "-ref"))
+                       struct)
+                (vector-ref struct ,offset)))
+           (set! offset (+ 1 offset))
+           func)
+         slot-names)
+        )))
+
