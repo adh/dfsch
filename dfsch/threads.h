848d6cfc4f704a446eba119c3440acdc4b907514
Ales Hakl <ales@hakl.net>
2006-07-13 12:42:10 +0000

    2006-07-13  Ales Hakl  <ales at hakl dot net>
    +	* src/threads.c: Mutexes (non-working)
    +
     	* src/dfsch.c (dfsch_call_ec): Removed volatile (there is no
     	reason for it to be there, probably)
     	(dfsch_eval_proc_tr): dtto.
    
    
    git-svn-id: svn://mahoro/dfsch/trunk@409 876e91b9-e3f0-0310-bb80-9da7c39ee78d

--- dfsch/threads.h
+++ dfsch/threads.h
@@ -5,11 +5,15 @@
 
 extern dfsch_object_t* dfsch_thread_create(dfsch_object_t* function,
                                            dfsch_object_t* arguments);
-
 extern dfsch_object_t* dfsch_thread_join(dfsch_object_t* thread);
-extern dfsch_object_t* dfsch_thread_detach(dfsch_object_t* thread);
-
+extern void dfsch_thread_detach(dfsch_object_t* thread);
 extern dfsch_object_t* dfsch_thread_self();
+
+extern dfsch_object_t* dfsch_mutex_create();
+extern void dfsch_mutex_lock(dfsch_object_t* mutex);
+extern int dfsch_mutex_trylock(dfsch_object_t* mutex);
+extern void dfsch_mutex_unlock(dfsch_object_t* mutex);
+
 
 
 extern dfsch_object_t* dfsch_threads_register(dfsch_ctx_t *ctx);
--- src/threads.c
+++ src/threads.c
@@ -48,29 +48,31 @@ dfsch_object_t* dfsch_thread_create(dfsch_object_t* function,
 dfsch_object_t* dfsch_thread_join(dfsch_object_t* thread){
   thread_t* t;
   dfsch_object_t* ret;
+  int err;
   if (!thread || thread->type != &thread_type)
     dfsch_throw("thread:not-a-thread", thread);
 
   t = (thread_t*)thread;
 
-  if (pthread_join(t->thread, (void*)&ret) != 0){
-    int err = errno;
+  err = pthread_join(t->thread, (void*)&ret);
+
+  if (err != 0){
     dfsch_throw("thread:unix-error",dfsch_make_string_cstr(strerror(err)));
   }
 
   return ret;
 }
-dfsch_object_t* dfsch_thread_detach(dfsch_object_t* thread){
+void dfsch_thread_detach(dfsch_object_t* thread){
   thread_t* t;
+  int err;
   if (!thread || thread->type != &thread_type)
     dfsch_throw("thread:not-a-thread", thread);
 
-  if (pthread_detach(t->thread) != 0){
-    int err = errno;
+  err = pthread_detach(t->thread);
+
+  if (err != 0){
     dfsch_throw("thread:unix-error",dfsch_make_string_cstr(strerror(err)));
   }
-
-  return thread;
 }
 
 dfsch_object_t* dfsch_thread_self(){
@@ -80,6 +82,78 @@ dfsch_object_t* dfsch_thread_self(){
 
   return (dfsch_object_t*)thread;
 }
+
+// Mutexes
+
+typedef struct mutex_t {
+  dfsch_type_t* type;
+  pthread_mutex_t mutex;
+} mutex_t;
+
+static const dfsch_type_t mutex_type = {
+  sizeof(mutex_t), 
+  "mutex",
+  NULL,
+  NULL
+};
+
+static mutex_finalizer(mutex_t* mutex, void* cd){
+  /*
+   * When given mutex is locked we will do nothing. If user loses reference
+   * to locked mutex, something is probably wrong.
+   *
+   * By the way, on LinuxThreads this is nevertheless a little more than 
+   * no-op, so it probably doesn't matter. (althought some other 
+   * impementations may leak some resources here)
+   */
+
+  //  pthread_mutex_destroy(&(mutex->mutex));
+}
+
+dfsch_object_t* dfsch_mutex_create(){
+  mutex_t* mutex = (mutex_t*)dfsch_make_object(&mutex_type);
+
+  GC_REGISTER_FINALIZER(mutex, 
+                        (GC_finalization_proc)mutex_finalizer,
+                        NULL, NULL, NULL);
+
+  pthread_mutex_init(&(mutex->mutex), NULL);
+
+  return (dfsch_object_t*)mutex;
+}
+void dfsch_mutex_lock(dfsch_object_t* mutex){
+  mutex_t* m;
+  int err;
+  if (!mutex || mutex->type != &mutex_type)
+    dfsch_throw("thread:not-a-mutex", mutex);
+
+  err = pthread_mutex_lock(&(m->mutex));
+
+  if (err != 0){
+    dfsch_throw("thread:unix-error",dfsch_make_string_cstr(strerror(err)));
+  }
+}
+int dfsch_mutex_trylock(dfsch_object_t* mutex){
+  mutex_t* m;
+  if (!mutex || mutex->type != &mutex_type)
+    dfsch_throw("thread:not-a-mutex", mutex);
+
+  return (pthread_mutex_trylock(&(m->mutex)) == 0);
+}
+void dfsch_mutex_unlock(dfsch_object_t* mutex){
+  mutex_t* m;
+  int err;
+  if (!mutex || mutex->type != &mutex_type)
+    dfsch_throw("thread:not-a-mutex", mutex);
+
+  err = pthread_mutex_unlock(&(m->mutex));
+
+  if (err != 0){
+    int err = errno;
+    dfsch_throw("thread:unix-error",dfsch_make_string_cstr(strerror(err)));
+  }
+}
+
 
 
 static dfsch_object_t* native_thread_create(void*baton, dfsch_object_t* args, 
@@ -107,13 +181,47 @@ static dfsch_object_t* native_thread_detach(void*baton, dfsch_object_t* args,
   DFSCH_OBJECT_ARG(args, thread);
   DFSCH_ARG_END(args);
 
-  return dfsch_thread_join(thread);
+  dfsch_thread_detach(thread);
+  return thread;
 }
 static dfsch_object_t* native_thread_self(void*baton, dfsch_object_t* args, 
                                           dfsch_tail_escape_t* esc){
   DFSCH_ARG_END(args);
 
   return dfsch_thread_self();
+}
+
+static dfsch_object_t* native_mutex_create(void*baton, dfsch_object_t* args, 
+                                           dfsch_tail_escape_t* esc){
+  DFSCH_ARG_END(args);
+
+  return dfsch_mutex_create();
+}
+static dfsch_object_t* native_mutex_lock(void*baton, dfsch_object_t* args, 
+                                         dfsch_tail_escape_t* esc){
+  dfsch_object_t* mutex;
+  DFSCH_OBJECT_ARG(args, mutex);
+  DFSCH_ARG_END(args);
+
+  dfsch_mutex_lock(mutex);
+  return mutex;
+}
+static dfsch_object_t* native_mutex_trylock(void*baton, dfsch_object_t* args, 
+                                         dfsch_tail_escape_t* esc){
+  dfsch_object_t* mutex;
+  DFSCH_OBJECT_ARG(args, mutex);
+  DFSCH_ARG_END(args);
+
+  return dfsch_bool(dfsch_mutex_trylock(mutex));
+}
+static dfsch_object_t* native_mutex_unlock(void*baton, dfsch_object_t* args, 
+                                           dfsch_tail_escape_t* esc){
+  dfsch_object_t* mutex;
+  DFSCH_OBJECT_ARG(args, mutex);
+  DFSCH_ARG_END(args);
+
+  dfsch_mutex_unlock(mutex);
+  return mutex;
 }
 
 
@@ -126,5 +234,15 @@ dfsch_object_t* dfsch_threads_register(dfsch_ctx_t *ctx){
                    dfsch_make_primitive(&native_thread_detach,NULL));
   dfsch_ctx_define(ctx, "thread:self", 
                    dfsch_make_primitive(&native_thread_self,NULL));
+
+  dfsch_ctx_define(ctx, "mutex:create", 
+                   dfsch_make_primitive(&native_mutex_create,NULL));
+  dfsch_ctx_define(ctx, "mutex:lock", 
+                   dfsch_make_primitive(&native_mutex_lock,NULL));
+  dfsch_ctx_define(ctx, "mutex:trylock", 
+                   dfsch_make_primitive(&native_mutex_trylock,NULL));
+  dfsch_ctx_define(ctx, "mutex:unlock", 
+                   dfsch_make_primitive(&native_mutex_unlock,NULL));
+
   return NULL;
 }
