7964d3be5c824deed3427f644edf5cdfe9735a78
Ales Hakl <ales@hakl.net>
2006-10-21 20:47:08 +0000

    2006-10-21  Ales Hakl  <ales at hakl dot net>
     	* scm/test.scm (strings): Some tests for strings.
    +	(*utf8*): utf-8 related tests 
     
     	* src/dfsch.c (dfsch_apply_impl): typeless-value can be applied
     	and evaluates to empty list (quite useless feature)
    
    
    git-svn-id: svn://mahoro/dfsch/trunk@471 876e91b9-e3f0-0310-bb80-9da7c39ee78d

--- scm/test.scm
+++ scm/test.scm
@@ -214,7 +214,10 @@
 
 (test 'append (string-append "abc" "def") "abcdef")
 (test 'substring (substring "abcdef" 2 4) "cd")
-
+(test 'utf8-ref (string-utf8-ref "ab©" 2) 169)
+(test 'utf8->list (string-utf8->list "ab©") '(97 98 169))
+(test 'list->utf8 (list->string-utf8 '(0x3042 0x3044 0x3046 0x3048 0x304a))
+      "あいうえお")
 
 (group "some special cases")
 
