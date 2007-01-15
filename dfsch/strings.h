1445f9774e1a49b1800d9566313f29eca1f9fc21
Ales Hakl <ales@hakl.net>
2007-01-15 15:31:57 +0000

    2007-01-15  Ales Hakl  <ales at hakl dot net>
    +	* dfsch/wrapper.h, dfsch/strings.h: added extern "C" for C++
    +
     	* src/parse.c (dispatch_string): unicode escapes
     
     	* src/strings.c (escape_table): excape NUL as \x00 and not \0
    
    
    git-svn-id: svn://mahoro/dfsch/trunk@502 876e91b9-e3f0-0310-bb80-9da7c39ee78d

--- dfsch/strings.h
+++ dfsch/strings.h
@@ -3,75 +3,82 @@
 
 #include <dfsch/dfsch.h>
 
-typedef struct dfsch_strbuf_t{
-  char* ptr;
-  size_t len;
-} dfsch_strbuf_t;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-extern int dfsch_string_p(dfsch_object_t* obj);
+  typedef struct dfsch_strbuf_t{
+    char* ptr;
+    size_t len;
+  } dfsch_strbuf_t;
 
-extern dfsch_strbuf_t* dfsch_strbuf_create(char* ptr, size_t len);
+  extern int dfsch_string_p(dfsch_object_t* obj);
 
-extern dfsch_object_t* dfsch_make_string_cstr(char* string);
-extern dfsch_object_t* dfsch_make_string_strbuf(dfsch_strbuf_t* strbuf);
-extern dfsch_object_t* dfsch_make_string_buf(char* ptr, size_t len);
+  extern dfsch_strbuf_t* dfsch_strbuf_create(char* ptr, size_t len);
+  
+  extern dfsch_object_t* dfsch_make_string_cstr(char* string);
+  extern dfsch_object_t* dfsch_make_string_strbuf(dfsch_strbuf_t* strbuf);
+  extern dfsch_object_t* dfsch_make_string_buf(char* ptr, size_t len);
 
-extern char* dfsch_string_to_cstr(dfsch_object_t* obj);
-extern dfsch_strbuf_t* dfsch_string_to_buf(dfsch_object_t* obj);
+  extern char* dfsch_string_to_cstr(dfsch_object_t* obj);
+  extern dfsch_strbuf_t* dfsch_string_to_buf(dfsch_object_t* obj);
 
-extern int dfsch_string_cmp(dfsch_object_t* a, dfsch_object_t* b);
-extern int dfsch_string_eq_p(dfsch_object_t* a, dfsch_object_t* b);
-extern int dfsch_string_lt_p(dfsch_object_t* a, dfsch_object_t* b);
-extern int dfsch_string_gt_p(dfsch_object_t* a, dfsch_object_t* b);
-extern int dfsch_string_lte_p(dfsch_object_t* a, dfsch_object_t* b);
-extern int dfsch_string_gte_p(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_cmp(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_eq_p(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_lt_p(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_gt_p(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_lte_p(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_gte_p(dfsch_object_t* a, dfsch_object_t* b);
 
-extern int dfsch_string_cmp_ci(dfsch_object_t* a, dfsch_object_t* b);
-extern int dfsch_string_ci_eq_p(dfsch_object_t* a, dfsch_object_t* b);
-extern int dfsch_string_ci_lt_p(dfsch_object_t* a, dfsch_object_t* b);
-extern int dfsch_string_ci_gt_p(dfsch_object_t* a, dfsch_object_t* b);
-extern int dfsch_string_ci_lte_p(dfsch_object_t* a, dfsch_object_t* b);
-extern int dfsch_string_ci_gte_p(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_cmp_ci(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_ci_eq_p(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_ci_lt_p(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_ci_gt_p(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_ci_lte_p(dfsch_object_t* a, dfsch_object_t* b);
+  extern int dfsch_string_ci_gte_p(dfsch_object_t* a, dfsch_object_t* b);
 
-extern dfsch_object_t* dfsch_string_list_append(dfsch_object_t* list);
+  extern dfsch_object_t* dfsch_string_list_append(dfsch_object_t* list);
 
-extern char dfsch_string_ref(dfsch_object_t* string, size_t index);
+  extern char dfsch_string_ref(dfsch_object_t* string, size_t index);
 
-extern size_t dfsch_string_length(dfsch_object_t* string);
+  extern size_t dfsch_string_length(dfsch_object_t* string);
 
-extern dfsch_object_t* dfsch_string_substring(dfsch_object_t* string, 
-                                              size_t start,
-                                              size_t end);
+  extern dfsch_object_t* dfsch_string_substring(dfsch_object_t* string, 
+                                                size_t start,
+                                                size_t end);
 
-extern dfsch_object_t* dfsch_string_substring_utf8(dfsch_object_t* string, 
-                                                   size_t start,
-                                                   size_t end);
+  extern dfsch_object_t* dfsch_string_substring_utf8(dfsch_object_t* string, 
+                                                     size_t start,
+                                                     size_t end);
 
-extern dfsch_object_t* dfsch_string_2_list(dfsch_object_t* string);
+  extern dfsch_object_t* dfsch_string_2_list(dfsch_object_t* string);
 
 
-typedef int (*dfsch_string_callback_t)(char, void*);
-typedef int (*dfsch_string_unicode_callback_t)(uint32_t ch, 
-                                               void* baton,
-                                               size_t start,
-                                               size_t end);
-typedef int (*dfsch_string_unicode_invalid_callback_t)(uint32_t ch, 
-                                                       void* baton,
-                                                       size_t index);
+  typedef int (*dfsch_string_callback_t)(char, void*);
+  typedef int (*dfsch_string_unicode_callback_t)(uint32_t ch, 
+                                                 void* baton,
+                                                 size_t start,
+                                                 size_t end);
+  typedef int (*dfsch_string_unicode_invalid_callback_t)(uint32_t ch, 
+                                                         void* baton,
+                                                         size_t index);
 
-extern int dfsch_string_for_each(dfsch_string_callback_t proc,
-                                 dfsch_object_t* string,
-                                 void *baton);
+  extern int dfsch_string_for_each(dfsch_string_callback_t proc,
+                                   dfsch_object_t* string,
+                                   void *baton);
 
-extern int dfsch_string_utf8_for_each(dfsch_string_unicode_callback_t proc,
-                                      dfsch_object_t* string,
-                                      void *baton,
-                                      dfsch_string_unicode_invalid_callback_t iproc,
-                                      void* ibaton);
+  extern int dfsch_string_utf8_for_each(dfsch_string_unicode_callback_t proc,
+                                        dfsch_object_t* string,
+                                        void *baton,
+                                        dfsch_string_unicode_invalid_callback_t iproc,
+                                        void* ibaton);
 #define DFSCH_STRING_ARG(al, name) \
   DFSCH_GENERIC_ARG(al, name, char*, dfsch_string_to_cstr)
 #define DFSCH_STRING_ARG_OPT(al, name, default) \
   DFSCH_GENERIC_ARG(al, name, default, char*, dfsch_string_to_cstr)
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- dfsch/wrapper.h
+++ dfsch/wrapper.h
@@ -3,14 +3,22 @@
 
 #include <dfsch/dfsch.h>
 
-extern dfsch_object_t* dfsch_make_wrapper_type(char* name,
-                                               dfsch_object_t* write,
-                                               dfsch_object_t* equal_p,
-                                               dfsch_object_t* apply);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-extern dfsch_object_t* dfsch_wrap(dfsch_object_t* type,
-                                  dfsch_object_t* object);
-extern dfsch_object_t* dfsch_unwrap(dfsch_object_t* type,
-                                    dfsch_object_t* wrapper);
+  extern dfsch_object_t* dfsch_make_wrapper_type(char* name,
+                                                 dfsch_object_t* write,
+                                                 dfsch_object_t* equal_p,
+                                                 dfsch_object_t* apply);
+  
+  extern dfsch_object_t* dfsch_wrap(dfsch_object_t* type,
+                                    dfsch_object_t* object);
+  extern dfsch_object_t* dfsch_unwrap(dfsch_object_t* type,
+                                      dfsch_object_t* wrapper);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
