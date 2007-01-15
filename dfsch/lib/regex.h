2cf6226c2bbd4a26644f352ea9cad9740558b997
Ales Hakl <ales@hakl.net>
2007-01-15 15:33:41 +0000

    git-svn-id: svn://mahoro/dfsch/trunk@503 876e91b9-e3f0-0310-bb80-9da7c39ee78d

--- dfsch/lib/regex.h
+++ dfsch/lib/regex.h
@@ -4,11 +4,19 @@
 #include <dfsch/dfsch.h>
 #include <regex.h>
 
-dfsch_object_t* dfsch_regex_compile(char* expression, int flags);
-int dfsch_regex_match_p(dfsch_object_t* regex, char* string, int flags);
-dfsch_object_t* dfsch_regex_substrings(dfsch_object_t* regex, char* string,
-                                       int flags);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-dfsch_object_t* dfsch_regex_register(dfsch_object_t *ctx);
+  dfsch_object_t* dfsch_regex_compile(char* expression, int flags);
+  int dfsch_regex_match_p(dfsch_object_t* regex, char* string, int flags);
+  dfsch_object_t* dfsch_regex_substrings(dfsch_object_t* regex, char* string,
+                                         int flags);
+
+  dfsch_object_t* dfsch_regex_register(dfsch_object_t *ctx);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- dfsch/lib/threads.h
+++ dfsch/lib/threads.h
@@ -2,29 +2,36 @@
 #define H__dfsch__threads__
 
 #include <dfsch/dfsch.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-extern dfsch_object_t* dfsch_thread_create(dfsch_object_t* function,
-                                           dfsch_object_t* arguments);
-extern dfsch_object_t* dfsch_thread_join(dfsch_object_t* thread);
-extern void dfsch_thread_detach(dfsch_object_t* thread);
-extern dfsch_object_t* dfsch_thread_self();
+  extern dfsch_object_t* dfsch_thread_create(dfsch_object_t* function,
+                                             dfsch_object_t* arguments);
+  extern dfsch_object_t* dfsch_thread_join(dfsch_object_t* thread);
+  extern void dfsch_thread_detach(dfsch_object_t* thread);
+  extern dfsch_object_t* dfsch_thread_self();
 
-extern dfsch_object_t* dfsch_mutex_create();
-extern void dfsch_mutex_lock(dfsch_object_t* mutex);
-extern int dfsch_mutex_trylock(dfsch_object_t* mutex);
-extern void dfsch_mutex_unlock(dfsch_object_t* mutex);
+  extern dfsch_object_t* dfsch_mutex_create();
+  extern void dfsch_mutex_lock(dfsch_object_t* mutex);
+  extern int dfsch_mutex_trylock(dfsch_object_t* mutex);
+  extern void dfsch_mutex_unlock(dfsch_object_t* mutex);
 
-extern dfsch_object_t* dfsch_condition_create();
-extern void dfsch_condition_wait(dfsch_object_t* condition,
-                                 dfsch_object_t* mutex);
-extern void dfsch_condition_signal(dfsch_object_t* condition);
-extern void dfsch_condition_broadcast(dfsch_object_t* condition);
+  extern dfsch_object_t* dfsch_condition_create();
+  extern void dfsch_condition_wait(dfsch_object_t* condition,
+                                   dfsch_object_t* mutex);
+  extern void dfsch_condition_signal(dfsch_object_t* condition);
+  extern void dfsch_condition_broadcast(dfsch_object_t* condition);
 
-extern dfsch_object_t* dfsch_channel_create(size_t buffer);
-extern dfsch_object_t* dfsch_channel_read(dfsch_object_t* channel);
-extern void dfsch_channel_write(dfsch_object_t* channel,
-                                dfsch_object_t* object);
+  extern dfsch_object_t* dfsch_channel_create(size_t buffer);
+  extern dfsch_object_t* dfsch_channel_read(dfsch_object_t* channel);
+  extern void dfsch_channel_write(dfsch_object_t* channel,
+                                  dfsch_object_t* object);
 
-extern dfsch_object_t* dfsch_threads_register(dfsch_object_t *ctx);
+  extern dfsch_object_t* dfsch_threads_register(dfsch_object_t *ctx);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
