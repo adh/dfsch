789b2deadb368dd93db14ae37f90c877b03c2e26
Ales Hakl <ales@hakl.net>
2007-08-17 21:59:12 +0000

    +2007-08-17  Ales Hakl  <ales at hakl dot net> + +	* dfsch/lib/regex.h: Documentation comments +  2007-08-13  Ales Hakl  <ales at hakl dot net>
     	* lib/load.c (dfsch_load): fixed path searching logic.
    
    
    git-svn-id: svn://mahoro/dfsch/trunk@573 876e91b9-e3f0-0310-bb80-9da7c39ee78d

--- dfsch/lib/load.h
+++ dfsch/lib/load.h
@@ -35,10 +35,12 @@
 extern "C" {
 #endif
 
-  /** Load scheme file into given context. Search for file in directories named
-      in scheme variable load:path. File is loaded into top level environment 
-      of context and only top level definition of load:path is taken into 
-      account */
+  /** 
+   * Load scheme file into given context. Search for file in directories named
+   * in scheme variable load:path. File is loaded into top level environment 
+   * of context and only top level definition of load:path is taken into 
+   * account 
+   */
   extern dfsch_object_t* dfsch_load_scm(dfsch_object_t* ctx, char* scm_name);
   /** Append directory to load:path */
   dfsch_object_t* dfsch_load_extend_path(dfsch_object_t* ctx, char* dir);
--- dfsch/lib/regex.h
+++ dfsch/lib/regex.h
@@ -8,11 +8,29 @@
 extern "C" {
 #endif
 
+  /**
+   * Compile given regular expression into regex object. Flags are same as 
+   * for regcomp(3).
+   */
   dfsch_object_t* dfsch_regex_compile(char* expression, int flags);
+
+  /**
+   * Apply precompiled regex object to given string and return whetever 
+   * regular expression matches
+   */
   int dfsch_regex_match_p(dfsch_object_t* regex, char* string, int flags);
+
+  /**
+   * Apply precompiled regex object to given string and return vector of
+   * matching substrings (each as vector of start offset, end offset and 
+   * matching string).
+   */
   dfsch_object_t* dfsch_regex_substrings(dfsch_object_t* regex, char* string,
                                          int flags);
 
+  /**
+   * Define regular expression primitives in given environment.
+   */
   dfsch_object_t* dfsch_regex_register(dfsch_object_t *ctx);
 
 #ifdef __cplusplus
