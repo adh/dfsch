5ec5013933b5e201f3e7f4902531cf049728da74
Ales Hakl <ales@hakl.net>
2008-07-31 14:46:02 +0000

    git-svn-id: svn://mahoro/dfsch/trunk@896 876e91b9-e3f0-0310-bb80-9da7c39ee78d

--- src/repl.c
+++ src/repl.c
@@ -371,7 +371,7 @@ int main(int argc, char**argv){
       break;
     case 'v':
       printf("dfsch version %s\n\n", PACKAGE_VERSION);
-      puts("Copyright (C) 2005-2007 Ales Hakl");
+      puts("Copyright (C) 2005-2008 Ales Hakl");
       puts("dfsch comes with ABSOLUTELY NO WARRANTY");
       puts("This is free software, and you are welcome to redistribute it");
       puts("under certain conditions; see file COPYING for details.");
