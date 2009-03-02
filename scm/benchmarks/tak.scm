ad65b8ffe67b903c03618bb311c1c8424c86044b
Ales Hakl <ales@hakl.net>
2009-03-02 03:03:33 +0100

    Fix tak-inline benchmark

--- scm/benchmarks/tak.scm
+++ scm/benchmarks/tak.scm
@@ -12,9 +12,9 @@
 (define (tak-inline x y z)
   (#.if (#.not (#.< y x))
       z
-      (tak (tak (#.- x 1) y z)
-           (tak (#.- y 1) z x)
-           (tak (#.- z 1) x y))))
+      (tak-inline (tak-inline (#.- x 1) y z)
+                  (tak-inline (#.- y 1) z x)
+                  (tak-inline (#.- z 1) x y))))
 
 
 (define-macro (measure-time name . body)
