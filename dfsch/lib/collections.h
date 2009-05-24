758c13cab1bb7d38ed4ac19071e504efbf675f95
Ales Hakl <ales@hakl.net>
2009-05-25 00:08:00 +0200

    bitvector interface

--- dfsch/lib/collections.h
+++ dfsch/lib/collections.h
@@ -13,5 +13,21 @@ void dfsch_collections_priority_queue_push(dfsch_object_t* q,
 dfsch_object_t* dfsch_collections_priority_queue_pop(dfsch_object_t* q);
 int dfsch_collections_priority_queue_empty_p(dfsch_object_t* q);
 
+extern dfsch_type_t dfsch_collections_bitvector_type;
+#define DFSCH_COLLECTIONS_BITVECTOR_TYPE (&dfsch_collections_bitvector_type)
+
+dfsch_object_t* dfsch_collections_make_bitvector(size_t length);
+int dfsch_collections_bitvector_ref(dfsch_object_t* bv, size_t n);
+void dfsch_collections_bitvector_set(dfsch_object_t* bv, size_t n, int v);
+size_t dfsch_collections_bitvector_length(dfsch_object_t* bv);
+dfsch_object_t* dfsch_collections_bitvector_not(dfsch_object_t* bv);
+dfsch_object_t* dfsch_collections_bitvector_or(dfsch_object_t* bva,
+                                               dfsch_object_t* bvb);
+dfsch_object_t* dfsch_collections_bitvector_and(dfsch_object_t* bva,
+                                               dfsch_object_t* bvb);
+dfsch_object_t* dfsch_collections_bitvector_xor(dfsch_object_t* bva,
+                                                dfsch_object_t* bvb);
+dfsch_object_t* dfsch_collections_bitvector_2_integer(dfsch_object_t* bv);
+dfsch_object_t* dfsch_collections_integer_2_bitvector(dfsch_object_t* bv);
 
 #endif
