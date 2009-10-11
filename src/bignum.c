9a50937591fa76ae41119e478ff612e84b12a850
Ales Hakl <ales@hakl.net>
2009-10-11 02:07:23 +0200

    Revert "fixed base check in bignum_exp"
    This reverts commit 454eeccf243d6db5736332da8d29b714cff3f858.

--- src/bignum.c
+++ src/bignum.c
@@ -749,7 +749,7 @@ bignum_t* dfsch_bignum_exp(bignum_t* b, bignum_t* e, bignum_t* m){
   bignum_t* r;
   size_t i;
 
-  if (b && b->negative){
+  if (m && b->negative){
     dfsch_error("Negative base for modular exponentation", NULL);
   }
   if (m && m->length == 0){
