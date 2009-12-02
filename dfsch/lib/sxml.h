403eb4499d362fb13a32c31d0e1d12d92e85b0c1
Ales Hakl <ales@hakl.net>
2009-12-02 13:46:11 +0100

    fix indetation

--- dfsch/lib/sxml.h
+++ dfsch/lib/sxml.h
@@ -9,19 +9,18 @@ typedef struct dfsch_sxml_parser_params_t {
   int collapse_whitespace;
 } dfsch_sxml_parser_params_t;
 
-dfsch_sxml_parser_params_t* 
-dfsch_sxml_parser_params(dfsch_object_t* args);
+dfsch_sxml_parser_params_t* dfsch_sxml_parser_params(dfsch_object_t* args);
 
 dfsch_object_t* dfsch_sxml_parse_file(char* filename,
-                                          dfsch_sxml_parser_params_t* params);
+                                      dfsch_sxml_parser_params_t* params);
 dfsch_object_t* dfsch_sxml_parse_port(dfsch_object_t* port,
-                                          dfsch_sxml_parser_params_t* params);
+                                      dfsch_sxml_parser_params_t* params);
 dfsch_object_t* dfsch_sxml_parse_buf(char* buf, size_t len,
-                                         dfsch_sxml_parser_params_t* params);
+                                     dfsch_sxml_parser_params_t* params);
 dfsch_object_t* dfsch_sxml_parse_strbuf(dfsch_strbuf_t* b,
-                                            dfsch_sxml_parser_params_t* params);
+                                        dfsch_sxml_parser_params_t* params);
 dfsch_object_t* dfsch_sxml_parse_cstr(char* s,
-                                          dfsch_sxml_parser_params_t* params);
+                                      dfsch_sxml_parser_params_t* params);
 
 typedef struct dfsch_sxml_emitter_params_t {
   int pretty_print;
@@ -35,10 +34,10 @@ dfsch_sxml_emitter_params_t* dfsch_sxml_emitter_params(dfsch_object_t* args);
 
 
 char* dfsch_sxml_emit_cstr(dfsch_object_t* infoset,
-                               dfsch_sxml_emitter_params_t* params);
+                           dfsch_sxml_emitter_params_t* params);
 void dfsch_sxml_emit_port(dfsch_object_t* infoset, dfsch_object_t* port,
-                              dfsch_sxml_emitter_params_t* params);
+                          dfsch_sxml_emitter_params_t* params);
 void dfsch_sxml_emit_file(dfsch_object_t* infoset, char* filename,
-                              dfsch_sxml_emitter_params_t* params);
+                          dfsch_sxml_emitter_params_t* params);
 
 #endif
