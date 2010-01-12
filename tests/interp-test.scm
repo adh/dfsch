0f49f7be3d81d712edcc8f5b334946411f0e5199
Ales Hakl <ales@hakl.net>
2010-01-12 10:29:08 +0100

    test for named-let

--- tests/interp-test.scm
+++ tests/interp-test.scm
@@ -230,7 +230,21 @@
                                         ()
                                         (even? (- n 1))))))
                           (even? 88))
-                        true))
+                        true)
+                  (test 'named-let
+                        (let loop ((numbers '(3 -2 1 6 -5))
+                                   (nonneg '())
+                                   (neg '()))
+                          (cond ((null? numbers) (list nonneg neg))
+                                ((>= (car numbers) 0)
+                                 (loop (cdr numbers)
+                                       (cons (car numbers) nonneg)
+                                       neg))
+                                ((< (car numbers) 0)
+                                 (loop (cdr numbers)
+                                       nonneg
+                                       (cons (car numbers) neg)))))   
+                        '((6 1 3) (-5 -2))))
        (sub-group destructuring-bind
                   (test 'simple
                         (destructuring-bind (a b c) '(1 2 3)
