630dc0ed65f3657cc9d73cca022a9c64e4897664
Ales Hakl <ales@hakl.net>
2010-03-08 23:40:21 +0100

    cinspect stub

--- lib/cinspect.c
+++ lib/cinspect.c
@@ -3,9 +3,27 @@
 #include <dfsch/lib/console.h>
 #include <dfsch/introspect.h>
 
+typedef struct inspector_state_t {
+  dfsch_object_t* object_stack;
+
+  dfsch_object_t* current_object;
+  char* description;
+  dfsch_object_t* slot_list;
+
+  int slot_print_depth;
+} inspector_state_t*;
+
+static void push_object(inspector_state_t* is, dfsch_object_t* obj){
+  
+}
+
+
+static void redisplay_object(inspector_state_t* is){
+  
+}
 
 static void inspect_object(dfsch_object_t* obj){
-
+  
 }
 
 
