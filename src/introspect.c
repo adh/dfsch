0c2a90b95e3bd4204ce25b0fc7cb707f270e33d5
Ales Hakl <ales@hakl.net>
2010-03-17 16:15:35 +0100

    do not number sequence virtual slots

--- src/introspect.c
+++ src/introspect.c
@@ -178,14 +178,12 @@ dfsch_object_t* dfsch_describe_object(dfsch_object_t* obj){
 
   if (DFSCH_PAIR_P(obj)){
     if (dfsch_list_length(obj, NULL) > 0){
-      int j = 0;
       i = obj;
       while (DFSCH_PAIR_P(i)){
         dfsch_list_collect(lc, 
                            dfsch_list(2,
-                                      DFSCH_MAKE_FIXNUM(j),
+                                      NULL,
                                       DFSCH_FAST_CAR(i)));
-        j++;
         i = DFSCH_FAST_CDR(i);
       }
 
