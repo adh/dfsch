28e93059082f8b40357f4414292597f9fa59fee7
Ales Hakl <ales@hakl.net>
2010-07-10 10:52:24 +0200

    windows fixes

--- lib/cdebug_mod.c
+++ lib/cdebug_mod.c
@@ -2,6 +2,6 @@
 #include <dfsch/lib/cdebug.h>
 
 dfsch_object_t* dfsch_module_cdebug_register(dfsch_object_t* env){
-  dfsch_package_t* cdebug_pkg = dfsch_define_package("cdebug");
+  dfsch_package_t* cdebug_pkg = dfsch_make_package("cdebug");
   dfsch_define_pkgcstr(env, cdebug_pkg, "debugger-procedure", dfsch_cdebug_get_procedure());
 }
