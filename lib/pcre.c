4ca23069fea15794b37e68c8264084ac0faded7b
Ales Hakl <ales@hakl.net>
2010-12-11 00:59:05 +0100

    common logic for handling return values

--- lib/pcre.c
+++ lib/pcre.c
@@ -3,7 +3,6 @@
 typedef struct pattern_t {
   dfsch_type_t* type;
   char* pattern;
-  int options;
   void* data[];
 } pattern_t;
 
@@ -40,9 +39,21 @@ dfsch_object_t* dfsch_pcre_compile(char* pattern,
 
   res = dfsch_make_object_var(DFSCH_PCRE_PATTERN_TYPE, len);
   memcpy(res->data, code, len);
+  res->pattern = dfsch_stracpy(pattern);
   pcre_free(code);
 
   return res;
+}
+
+static match_res(int res){
+  switch (res){
+  case PCRE_ERROR_NOMATCH:
+    return 0;
+  case 0:
+    return 1;
+  default:
+    dfsch_error("pcre_exec error", DFSCH_MAKE_FIXNUM(res));
+  }
 }
 
 int dfsch_pcre_match(pcre* pattern,
@@ -57,16 +68,10 @@ int dfsch_pcre_match(pcre* pattern,
   vs = backref * 3;
   int vec[vs];
 
-  res = pcre_exec(pattern, NULL, string, len, 0, options, vec, vs);
+  return match_res(pcre_exec(pattern, NULL, 
+                             string, len, 0, 
+                             options, vec, vs));
 
-  switch (res){
-  case PCRE_ERROR_NOMATCH:
-    return 0;
-  case 0:
-    return 1;
-  default:
-    dfsch_error("pcre_exec error", DFSCH_MAKE_FIXNUM(res));
-  }
 }
 dfsch_object_t* dfsch_pcre_match_substrings(pcre* pattern,
                                             char* string, size_t len,
