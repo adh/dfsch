db16ed6c5afa934eb4108af3d430f76078e70b42
Ales Hakl <ales@hakl.net>
2011-03-30 05:49:11 +0200

    fixed shadowing of global function

---
+++ tests/language-tests.scm
@@ -0,0 +1,54 @@
+(define-test test-eq? (:language :equality)
+  (assert-true (eq? 'a 'a))
+  (assert-false (eq? 'a 'b))
+  (assert-false (eq? (gensym) (gensym))))
+
+(define-test test-equal? (:language :equality)
+  (assert-true (equal? '(1 2 3) '(1 2 3)))
+  (assert-false (equal? '(1 2 3) '(1 2 5)))
+  (assert-true (equal? "foo" "foo"))
+  (assert-false (equal? "foo" "bar"))
+  (assert-true (equal? #(1 2 3) #(1 2 3)))
+  (assert-true (equal? '(1 2 ((3) 4 5)) '(1 2 ((3) 4 5))))
+  (assert-false (equal? '(1 2 ((3) 4 5)) '(1 2 ((7) 4 5)))))
+
+(define-test test-eqv? (:language :equality)
+  (assert-true (eqv? 1.34 1.34)))
+
+(define-test fixnum-arithmetics (:language :numbers)
+  (assert-equal (+ 3 4) 7)
+  (assert-equal (+ 3) 3)
+  (assert-equal (* 4) 4)
+  (assert-equal (*) 1)
+  
+  (assert-equal (- 3 4) -1)
+  (assert-equal (- 3 4 5) -6)
+  (assert-equal (- 3) -3)
+  (assert-equal (/ 3 4 5) (/ 3 20))
+  (assert-equal (/i 8 3) 2)
+  (assert-equal (mod-inv 3 11) 4)
+  
+  (assert-true (< 0 (+ most-positive-fixnum 1)))
+  (assert-true (> 0 (- most-negative-fixnum 1))))
+
+(define-test bignum-arithmetics (:language :numbers)
+  (assert-equal (integer-expt 1234567890123456789 
+                              789456123 
+                              102030405060708090)
+                67367859943785579)
+  (assert-equal (- 1234567890123456789012345678901234567890
+                   1234567890123456789012345678901234567891)
+                -1)
+  (assert-equal (* 65536 65536 65536 65536 65536 65536 65536 65536)
+                (* 256 256 256 256 256 256 256 256
+                   256 256 256 256 256 256 256 256))
+  (assert-equal (logior 123456789123456789 112233445566778899) 
+                125744530602155799)
+  (assert-equal (logxor 112233445566778899 123456789123456789)
+                15798826514075910)
+  (assert-equal (logand 112233445566778899 123456789123456789)
+                109945704088079889)
+  (assert-equal (>> 3523532227357930030104576 54) 
+                195595330))
+
+
---
+++ tests/main.scm
@@ -0,0 +1,21 @@
+(require :dfsch-unit)
+(use-package :dfsch-unit)
+
+(define-macro (define-evaluation-test name categories &rest exprs)
+  `(let ()
+     (define-test ,name ,categories
+       ,@(map (lambda (e)
+                (if (and (pair? e) (cdr e) (eq? (cadr e) '===>))
+                    (begin
+                      (when (not (cddr e))
+                            (error "Expected result missing" :clause e))
+                      (when (cdddr e)
+                            (error "Too many expressions in clause" :clause e))
+                      `(assert-equal ,(car e) ',(caddr e)))
+                    e))
+              exprs))))
+ 
+(require :language-tests)
+(require :r5rs-tests)
+
+(test-toplevel)
