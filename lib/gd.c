69edfdd6a94e25601582574766817fc26d4b93a7
Ales Hakl <ales@hakl.net>
2011-04-16 03:22:07 +0200

    wrapper layout

--- lib/gd.c
+++ lib/gd.c
@@ -1,5 +1,20 @@
 #include <dfsch/lib/gd.h>
 
+typedef struct gd_image_t {
+  dfsch_type_t* type;
+  gdImagePtr img;
+} gd_image_t;
+
+typedef struct gd_font_t {
+  dfsch_type_t* type;
+  gdFontPtr font;
+} gd_font_t;
+
+dfsch_type_t dfsch_gd_image_type = {};
+
+dfsch_type_t dfsch_gd_font_type = {};
+
+
 gdImagePtr dfsch_gd_image(dfsch_object_t* obj);
 dfsch_object_t* dfsch_gd_cons_image(gdImagePtr img);
 
