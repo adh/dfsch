b8f705516758e05759479fda7805e0d9b377406c
Ales Hakl <ales@hakl.net>
2011-05-29 16:32:54 +0200

    HTTP headers sending and receiving support, authentication

--- lib/curl.c
+++ lib/curl.c
@@ -3,6 +3,7 @@
 
 struct dfsch_curl_options_context_t {
   dfsch_strbuf_t* uploaded_data;
+  struct curl_slist* headers;
 };
 
 dfsch_curl_options_context_t* dfsch_curl_make_options_context(){
@@ -10,9 +11,31 @@ dfsch_curl_options_context_t* dfsch_curl_make_options_context(){
 }
 void dfsch_curl_cleanup(CURL* handle,
                         dfsch_curl_options_context_t* ctx){
+  if (ctx){
+    if (ctx->headers){
+      curl_slist_free_all(ctx->headers);
+    }
+  }
   curl_easy_cleanup(handle);
 }
 
+void dfsch_curl_set_http_headers(CURL* handle,
+                                 dfsch_object_t* headers,
+                                 dfsch_curl_options_context_t* ctx){
+  struct curl_slist* list = NULL;
+  dfsch_object_t* it = dfsch_collection_get_iterator(headers);
+
+  while (it){
+    dfsch_object_t* elem = dfsch_collection_this(it);
+    list = curl_slist_append(list, dfsch_string_to_cstr(elem));
+    it = dfsch_iterator_next(it);
+  }
+  
+  if (ctx){
+    ctx->headers = list;
+  }
+  curl_easy_xetopt(handle, CURLOPT_HTTPHEADER, list);
+}
 
 static size_t read_function(void* buf, size_t nmemb, size_t size, 
                             dfsch_strbuf_t* sb){
@@ -63,12 +86,35 @@ void dfsch_curl_setopt(CURL* handle,
     curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION,
                      value != NULL);
 
+  } else if (dfsch_compare_keyword(name, "output-headers")){
+    curl_easy_setopt(handle, CURLOPT_HEADER,
+                     value != NULL);
+
+  } else if (dfsch_compare_keyword(name, "headers")){
+    dfsch_curl_set_http_headers(handle, value, ctx);
+
   } else if (dfsch_compare_keyword(name, "username")){
     curl_easy_setopt(handle, CURLOPT_USERNAME, 
                      dfsch_string_to_cstr(value));
 
   } else if (dfsch_compare_keyword(name, "password")){
     curl_easy_setopt(handle, CURLOPT_PASSWORD, 
+                     dfsch_string_to_cstr(value));
+
+  } else if (dfsch_compare_keyword(name, "user-agent")){
+    curl_easy_setopt(handle, CURLOPT_USERAGENT, 
+                     dfsch_string_to_cstr(value));
+
+  } else if (dfsch_compare_keyword(name, "referer")){
+    curl_easy_setopt(handle, CURLOPT_REFERER, 
+                     dfsch_string_to_cstr(value));
+
+  } else if (dfsch_compare_keyword(name, "cookies")){
+    curl_easy_setopt(handle, CURLOPT_COOKIE, 
+                     dfsch_string_to_cstr(value));
+
+  } else if (dfsch_compare_keyword(name, "cookies")){
+    curl_easy_setopt(handle, CURLOPT_COOKIE, 
                      dfsch_string_to_cstr(value));
 
   } else {
