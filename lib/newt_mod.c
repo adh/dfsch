5bbd3f734dfb51c327d7f8060c7f562cb0cd528f
Ales Hakl <ales@hakl.net>
2011-12-11 19:01:30 +0100

    simple message boxes

--- lib/newt_mod.c
+++ lib/newt_mod.c
@@ -1,3 +1,4 @@
+#include <dfsch/lib/newt.h>
 #include <newt.h>
 #include <dfsch/dfsch.h>
 #include <dfsch/load.h>
@@ -100,6 +101,67 @@ DFSCH_DEFINE_PRIMITIVE(get_screen_size,
                     dfsch_make_number_from_long(rows));;
 }
 
+DFSCH_DEFINE_PRIMITIVE(win_message,
+                       "Creates message box window"){
+  char* title = NULL;
+  char* button_text = "OK";
+  char* text = NULL;
+
+  DFSCH_KEYWORD_PARSER_BEGIN(args);
+  DFSCH_KEYWORD_GENERIC("title", title, dfsch_string_to_cstr); 
+  DFSCH_KEYWORD_GENERIC("text", text, dfsch_string_to_cstr);
+  DFSCH_KEYWORD_GENERIC("button-text", button_text, dfsch_string_to_cstr);
+  DFSCH_KEYWORD_PARSER_END(args);
+
+  newtWinMessage(title, button_text, text, NULL);
+
+  return NULL;
+}
+
+DFSCH_DEFINE_PRIMITIVE(win_choice,
+                       "Creates message box window with two buttons"){
+  char* title = NULL;
+  char* button1_text = "Yes";
+  char* button2_text = "No";
+  char* text = NULL;
+
+  DFSCH_KEYWORD_PARSER_BEGIN(args);
+  DFSCH_KEYWORD_GENERIC("title", title, dfsch_string_to_cstr); 
+  DFSCH_KEYWORD_GENERIC("text", text, dfsch_string_to_cstr);
+  DFSCH_KEYWORD_GENERIC("button1-text", button1_text, dfsch_string_to_cstr);
+  DFSCH_KEYWORD_GENERIC("button2-text", button2_text, dfsch_string_to_cstr);
+  DFSCH_KEYWORD_PARSER_END(args);
+
+  return dfsch_make_number_from_long(newtWinChoice(title, 
+                                                   button1_text, 
+                                                   button2_text, 
+                                                   text, 
+                                                   NULL));
+}
+DFSCH_DEFINE_PRIMITIVE(win_ternary,
+                       "Creates message box window with three buttons"){
+  char* title = NULL;
+  char* button1_text = "Yes";
+  char* button2_text = "No";
+  char* button3_text = "Cancel";
+  char* text = NULL;
+
+  DFSCH_KEYWORD_PARSER_BEGIN(args);
+  DFSCH_KEYWORD_GENERIC("title", title, dfsch_string_to_cstr); 
+  DFSCH_KEYWORD_GENERIC("text", text, dfsch_string_to_cstr);
+  DFSCH_KEYWORD_GENERIC("button1-text", button1_text, dfsch_string_to_cstr);
+  DFSCH_KEYWORD_GENERIC("button2-text", button2_text, dfsch_string_to_cstr);
+  DFSCH_KEYWORD_GENERIC("button3-text", button3_text, dfsch_string_to_cstr);
+  DFSCH_KEYWORD_PARSER_END(args);
+
+  return dfsch_make_number_from_long(newtWinTernary(title, 
+                                                    button1_text, 
+                                                    button2_text, 
+                                                    button3_text, 
+                                                    text, 
+                                                    NULL));
+}
+
 
 void dfsch_module_newt_register(dfsch_object_t* env){
   dfsch_package_t* newt = dfsch_make_package("newt",
@@ -125,4 +187,10 @@ void dfsch_module_newt_register(dfsch_object_t* env){
                          DFSCH_PRIMITIVE_REF(pop_help_line));
   dfsch_defcanon_pkgcstr(env, newt, "get-screen-size", 
                          DFSCH_PRIMITIVE_REF(get_screen_size));
+  dfsch_defcanon_pkgcstr(env, newt, "win-message", 
+                         DFSCH_PRIMITIVE_REF(win_message));
+  dfsch_defcanon_pkgcstr(env, newt, "win-choice", 
+                         DFSCH_PRIMITIVE_REF(win_choice));
+  dfsch_defcanon_pkgcstr(env, newt, "win-ternary", 
+                         DFSCH_PRIMITIVE_REF(win_ternary));
 }
