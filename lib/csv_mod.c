51cc2fc4e97a5c9a7bfe8a90ec75b5eff367ce32
Ales Hakl <ales@hakl.net>
2011-12-16 00:18:24 +0100

    csv:read-file -> csv:read-port

--- lib/csv_mod.c
+++ lib/csv_mod.c
@@ -10,14 +10,24 @@ DFSCH_DEFINE_PRIMITIVE(read_line,
   return dfsch_csv_read_line(port, params);
 }
 
-DFSCH_DEFINE_PRIMITIVE(read_file,
-                       "Read CSV file into list of vectors"){
+DFSCH_DEFINE_PRIMITIVE(read_port,
+                       "Read CSV file from port into list of vectors"){
   dfsch_csv_params_t* params;
   dfsch_object_t* port;
   DFSCH_OBJECT_ARG(args, port);
   params = dfsch_csv_params(args);
 
   return dfsch_csv_read_file(port, params);  
+}
+
+DFSCH_DEFINE_PRIMITIVE(read_file,
+                       "Read CSV file into list of vectors"){
+  dfsch_csv_params_t* params;
+  char* filename;
+  DFSCH_STRING_ARG(args, filename);
+  params = dfsch_csv_params(args);
+
+  return dfsch_csv_read_file(dfsch_open_file_port(filename, "r"), params);  
 }
 
 void dfsch_module_csv_register(dfsch_object_t* env){
@@ -27,6 +37,8 @@ void dfsch_module_csv_register(dfsch_object_t* env){
   
   dfsch_defcanon_pkgcstr(env, csv, "read-line",
                          DFSCH_PRIMITIVE_REF(read_line));
+  dfsch_defcanon_pkgcstr(env, csv, "read-port",
+                         DFSCH_PRIMITIVE_REF(read_port));
   dfsch_defcanon_pkgcstr(env, csv, "read-file",
                          DFSCH_PRIMITIVE_REF(read_file));
 }
