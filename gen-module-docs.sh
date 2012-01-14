989420e4b3d1bc2ef26c6dcf02b885629f206c66
Ales Hakl <ales@hakl.net>
2012-01-14 16:31:40 +0100

    ...

--- gen-module-docs.sh
+++ gen-module-docs.sh
@@ -8,7 +8,7 @@ cat > documentation/modules/index.html <<EOF
 EOF
 
 if [ -f "$DOCGEN_HEAD_FILE" ]; then
-    cat "$DOCGEN_HEAD_FILE";
+    cat "$DOCGEN_HEAD_FILE" >> documentation/modules/index.html <<EOF
 fi
 
 cat >> documentation/modules/index.html <<EOF 
