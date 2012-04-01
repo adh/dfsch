b8969e72af4e1f204e351aefc38029b363827fa6
Ales Hakl <ales@hakl.net>
2012-04-01 06:37:16 +0200

    formating fix

--- tests/scm-test-interp.sh
+++ tests/scm-test-interp.sh
@@ -5,5 +5,6 @@ BUILD_DIR="`pwd`"
 cd $srcdir/tests
 
 
-$BUILD_DIR/dfsch-repl -X-compile_on_apply -L $BUILD_DIR/.libs -L ../lib-scm -L . main.scm
+$BUILD_DIR/dfsch-repl -X-compile_on_apply \
+    -L $BUILD_DIR/.libs -L ../lib-scm -L . main.scm
 
