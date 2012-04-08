f9e1879bc0fd18062f67030e07ffde6012317fe5
Ales Hakl <ales@hakl.net>
2012-04-08 09:58:49 +0200

    working queries

--- lib/sql/dbi.c
+++ lib/sql/dbi.c
@@ -83,12 +83,6 @@ static dfsch_object_t* cons_conn(dbi_conn conn){
 #define DBI_DATABASE_ARG(al, name)                              \
   DFSCH_INSTANCE_ARG(al, name, dbi_database_t*, &database_type)
 
-typedef struct dbi_result_t {
-  dfsch_type_t* type;
-  dbi_database_t* conn;
-  dbi_result result;
-} dbi_result_t;
-
 static void conn_error(dbi_conn conn){
   char* message;
   dbi_conn_error(conn, &message);
@@ -195,6 +189,116 @@ DFSCH_DEFINE_PRIMITIVE(exec_string,
   return NULL;
 }
 
+typedef struct dbi_result_t {
+  dfsch_type_t* type;
+  dbi_database_t* db;
+  dbi_result res;
+  dfsch_object_t* current;
+} dbi_result_t;
+
+
+static dfsch_object_t* row_as_vector(dbi_result res){
+  size_t i;
+  int n_columns = dbi_result_get_numfields(res);
+  dfsch_object_t* vec = dfsch_make_vector(n_columns, NULL);
+
+  for (i = 0; i < n_columns; i++){
+    dfsch_object_t* obj;
+
+    if (!dbi_result_field_is_null_idx(res, i + 1)){
+      switch (dbi_result_get_field_type_idx(res, i + 1)){
+      case DBI_TYPE_INTEGER:
+      case DBI_TYPE_DATETIME:
+        obj = dfsch_make_number_from_int64(dbi_result_get_longlong_idx(res, 
+                                                                       i + 1));
+        break;
+      case DBI_TYPE_DECIMAL:
+        obj = dfsch_make_number_from_string(dbi_result_get_string_idx(res, 
+                                                                      i + 1),
+                                            10);
+        break;
+      case DBI_TYPE_BINARY:
+        {
+          char* buf = dbi_result_get_binary_idx(res, i + 1);
+          obj = dfsch_make_byte_vector(buf, 
+                                       dbi_result_get_field_length_idx(res, 
+                                                                       i + 1));
+        }
+      default:
+        obj = dfsch_make_string_cstr(dbi_result_get_string_idx(res, i + 1));
+      }
+
+      dfsch_vector_set(vec, i, obj);
+    }
+
+  }
+
+  return vec;
+
+}
+
+static dfsch_object_t* result_next(dbi_result_t* res){
+  if (!dbi_result_next_row(res->res)){
+    dfsch_invalidate_object(res);
+    dbi_result_free(res->res);
+    return 0;
+  }
+  
+  res->current = row_as_vector(res->res);
+  return res;
+}
+static dfsch_object_t* result_this(dbi_result_t* res){
+  return res->current;
+}
+
+static dfsch_iterator_methods_t result_iterator = {
+  .next = result_next,
+  .this = result_this,
+};
+
+static dfsch_type_t dbi_result_type = {
+  .type = DFSCH_STANDARD_TYPE,
+  .superclass = NULL,
+  .size = sizeof(dbi_result_t),
+  .name = "dbi:result",
+  .collection = &dfsch_iterator_collection_methods,
+  .iterator = &result_iterator,
+};
+
+
+static void result_finalizer(dbi_result_t* res, void* discard){
+  if (res->type == &dbi_result_type){
+    dbi_result_free(res->res);
+  }
+}
+
+DFSCH_DEFINE_PRIMITIVE(query_string,
+                       "Execute query and return result object"
+                       DFSCH_DOC_SYNOPSIS("(db sql)")){
+  dbi_database_t* db;
+  char* sql;
+  dbi_result_t* res;
+  DBI_DATABASE_ARG(args, db);
+  DFSCH_STRING_ARG(args, sql);
+  DFSCH_ARG_END(args);
+  
+  res = dfsch_make_object(&dbi_result_type);
+
+  res->res = dbi_conn_query(db->conn, sql);
+
+  if (!res->res){
+    conn_error(db->conn);
+  }
+
+  res = result_next(res);
+  
+  if (res){
+    GC_register_finalizer(res, result_finalizer, NULL, NULL, NULL);
+  }
+  
+  return res;
+}
+
 
 void dfsch_module_dbi_register(dfsch_object_t* env){
   dfsch_package_t* dbi = dfsch_make_package("dbi",
@@ -218,4 +322,7 @@ void dfsch_module_dbi_register(dfsch_object_t* env){
   dfsch_define_method_pkgcstr(env, sql, "exec-string!",
                               NULL, dfsch_list(1, &database_type),
                               DFSCH_PRIMITIVE_REF(exec_string));
+  dfsch_define_method_pkgcstr(env, sql, "query-string",
+                              NULL, dfsch_list(1, &database_type),
+                              DFSCH_PRIMITIVE_REF(query_string));
 }
