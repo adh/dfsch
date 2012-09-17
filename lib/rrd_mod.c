dc1137ebf50368c7c06fce7a2d5dcce315e9cc99
Ales Hakl <ales@hakl.net>
2012-09-17 13:03:14 +0200

    argc/argv building helper functions

--- lib/rrd_mod.c
+++ lib/rrd_mod.c
@@ -2,6 +2,41 @@
 #include <dfsch/dfsch.h>
 #include <dfsch/load.h>
 
+static char* convert_arg(dfsch_object_t* obj){
+  if (dfsch_keyword_p(obj)){
+    char* str = dfsch_symbol(obj);
+    if (strlen(str) == 1){
+      return dfsch_saprintf("-%s", str);
+    } else {
+      return dfsch_saprintf("--%s", str);
+    }
+  } else if (dfsch_string_p(obj)) {
+    return dfsch_string_to_cstr(obj);
+  } else {
+    return dfsch_object_2_string(obj, 10, 1);
+  }
+}
+
+static void build_args(dfsch_object_t* list, int* pargc, char*** pargv){
+  int alloc = 16;
+  char** argv = GC_MALLOC(sizeof(char*) * alloc);
+  int argc = 0;
+  list = dfsch_collection_iterator(list);
+  
+  while (list){
+    if (alloc <= argc){
+      alloc *= 2;
+      argv = GC_REALLOC(argv, sizeof(char*) * alloc);
+    }
+
+    argv[argc] = convert_arg(dfsch_iterator_this(list));
+
+    argc++;
+    list = dfsch_iterator_next(list);
+  }
+  
+}
+
 void dfsch_module_rrd_register(dfsch_object_t* env){
   dfsch_package_t* rrd = dfsch_make_package("rrd",
                                             "Native library based interface to RRDtool");
